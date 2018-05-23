275492642dcbe511b199fc47714910e77d71d94c
Shiromegane <shiromegane42@gmail.com>
2018-05-24 03:18:34 +0900

    Updated

--- application/config/deploy/production.rb
+++ application/config/deploy/production.rb
@@ -3,20 +3,20 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '27.133.155.173', user: 'ubbuntu', roles: %w{app db web}
-server '27.133.155.173', user: 'ubbuntu', roles: %w{app web}
-server '27.133.155.173', user: 'ubbuntu', roles: %w{db}
+server '27.133.155.173', user: 'ubuntu', roles: %w{app db web}
+server '27.133.155.173', user: 'ubuntu', roles: %w{app web}
+server '27.133.155.173', user: 'ubuntu', roles: %w{db}
 
 # role :app, %w{27.133.155.173}
 # role :web, %w{27.133.155.173}
 # role :db,  %w{27.133.155.173}
 
 server '27.133.155.173',
-user: 'ubbuntu',
+user: 'ubuntu',
 roles: %w{web app},
 ssh_options: {
-  user: 'ubbuntu',
-  keys: %w(~/.ssh/jiraffe/id_rsa),
+  user: 'ubuntu',
+  keys: %w(~/.ssh/id_rsa),
   forward_agent: false,
   auth_methods: %w(publickey)
 }
